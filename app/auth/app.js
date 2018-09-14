592c1db3d59b0276ab7caf1ba2683b3b07619570
Riley Griffin <rigriffin241@gmail.com>
2018-09-14 15:46:47 -0400

    started site authentication

---
+++ app/auth/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const morgan = require('morgan')
+const path = require('path');
+c
+
+
+require('./config/passport');
+
+
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
+const flash = require('connect-flash');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const path = require('path');
 
 const app = express();
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost:27017/site-auth');
 
 app.use(morgan('dev'));
 
@@ -12,7 +23,6 @@ const getRoute = require('./app/routing/findArtilce.js');
 // routing for html
 const htmlRoute = require('./app/routing/htmlRoute.js');
     htmlRoute(app);
-
 
 const port = process.env.PORT || 3000;
 
